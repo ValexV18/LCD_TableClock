7d12f74eb989acf608546aeb8316743057052d82
ValexV18 <78489363+ValexV18@users.noreply.github.com>
2021-03-20 11:00:15 +0300

    clock with song

--- probnaya.ino
+++ probnaya.ino
@@ -1,9 +1,108 @@
-
+#define NOTE_C3  131
+#define NOTE_CS3 138
+#define NOTE_D3  147
+#define NOTE_DS3 155
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  496
+#define NOTE_C5  526
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define REST      0
 #include <RTClib.h>
 #include <LiquidCrystal.h>
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
+int tempo = 90;
+int buzzer = 5; 
+int melody[] = {
+
+  NOTE_D5,7, NOTE_E5,1,  //1
+  NOTE_G5,7, NOTE_C5,1,  //2
+  NOTE_A4,7, NOTE_E5,1,  //3
+  NOTE_C5,7, NOTE_G4,1,  //4
+
+  NOTE_G4,8, NOTE_G4,8, NOTE_G4,8, NOTE_F4,8, NOTE_E4,3, //1
+  NOTE_D4,8, NOTE_D4,8, NOTE_E4,8, NOTE_D4,5, NOTE_C4,3, //2
+  NOTE_G4,8, NOTE_G4,8, NOTE_G4,8, NOTE_A4,8, NOTE_G4,3, //3
+  NOTE_C4,8, NOTE_C4,6, NOTE_B4,2, //4
+  NOTE_G4,8, NOTE_G4,8, NOTE_G4,8, NOTE_F4,8, NOTE_E4,3, //1
+  NOTE_D4,8, NOTE_D4,8, NOTE_E4,8, NOTE_D4,5, NOTE_C4,3, //2
+  NOTE_G4,8, NOTE_G4,8, NOTE_G4,8, NOTE_A4,8, NOTE_G4,3, //3
+  NOTE_C4,8, NOTE_C4,6, NOTE_B4,2, //4
+
+  NOTE_C5,4, NOTE_B4,8, NOTE_A4,5, //1
+  NOTE_B4,8, NOTE_C5,8, NOTE_D5,5, 
+  NOTE_C5,8, NOTE_B4,7,            //2
+  NOTE_C5,8, NOTE_B4,8, NOTE_A4,5, 
+  NOTE_B4,8, NOTE_C5,7,            //3
+  NOTE_B4,8, NOTE_C5,8, NOTE_D5,5,
+  NOTE_C5,8, NOTE_D5,7, NOTE_E5,5, //4
+  NOTE_C5,4, NOTE_B4,8, NOTE_A4,5, //1
+  NOTE_B4,8, NOTE_C5,8, NOTE_D5,5, 
+  NOTE_C5,8, NOTE_B4,7,            //2
+  NOTE_C5,8, NOTE_B4,8, NOTE_A4,5, 
+  NOTE_B4,8, NOTE_C5,7,            //3
+  NOTE_B4,8, NOTE_C5,8, NOTE_D5,5,
+  NOTE_C5,8, NOTE_D5,7, NOTE_E5,5, NOTE_A4,1, //4
+
+  NOTE_B3,8, NOTE_G4,8, NOTE_E4,-1,  //1
+  NOTE_B3,8, NOTE_G4,8, NOTE_E4,-1,  //2
+  NOTE_B3,8, NOTE_G4,8, NOTE_E4,-1,  //3
+  NOTE_B3,8, NOTE_G4,8, NOTE_E4, 1,  //4
+
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //1
+  NOTE_FS4,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //2
+  NOTE_FS4,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //3
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,3, NOTE_FS4,5, //4
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //1
+  NOTE_FS4,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //2
+  NOTE_FS4,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //3
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,3, NOTE_FS4,5, //4
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //1
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //2
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //3
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,3, NOTE_FS4,5, //4
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //1
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //2
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,2,  //3
+  NOTE_B3,8, NOTE_E4,8, NOTE_FS4,8, NOTE_G4,3, NOTE_FS4,1, //4
+
+  NOTE_B3,8, NOTE_G4,8, NOTE_E4,-1,  //1
+  NOTE_B3,8, NOTE_G4,8, NOTE_E4,-1,  //2
+  NOTE_B3,8, NOTE_G4,8, NOTE_E4,-1,  //3
+  NOTE_B3,8, NOTE_G4,8, NOTE_E4, 1,  //4
+    
+};
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+int wholenote = (60000 * 4) / tempo;
+int divider = 0, noteDuration = 0;
+
 const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
 RTC_DS1307 RTC;
 DateTime now;
@@ -18,9 +117,21 @@ int behour;
 bool be_on = false;
 bool ON_OFF = false;
 
+
 void setup()
 {
   Serial.begin(9600);
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+    divider = melody[thisNote + 1];
+    if (divider > 0) 
+    {noteDuration = (wholenote) / divider;} 
+      else if (divider < 0) 
+      {noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5;}
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+    delay(noteDuration);
+    noTone(buzzer);
+  }
   lcd.begin(16, 2);
   delay(1000);
   pinMode(2, INPUT_PULLUP);
@@ -374,7 +485,7 @@ int display_page()
        {     
         delay(200);
         lcd.setCursor(0, 0);
-        lcd.print("\251c\277a\263\270\277e \263pe\274\307");
+        lcd.print("\251c\277a\275o\263\270\277e \263pe\274\307");
         if (!digitalRead(3))
         {select = (select-1);
         if (select < 0) select = 0;}
